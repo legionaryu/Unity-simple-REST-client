9459cc203d17c7952752258f62b9f2544ee7541a
Vitaliano <vitaliano.neto@gmail.com>
2016-01-21 11:24:27 -0200

    Created a button to remove a specific Key Value pair and a Clear output button POST method builds a form using WWForm from the Key Value pairs GET method uses the Key Values pairs to build a query string

--- Simple REST Client/Assets/Scripts/GUIHandler.cs
+++ Simple REST Client/Assets/Scripts/GUIHandler.cs
@@ -21,15 +21,95 @@ public class GUIHandler : MonoBehaviour {
     
     private IEnumerator SendCoroutine()
     {
-        WWW request = new WWW(urlInput.text);
-        yield return request;
-        if (string.IsNullOrEmpty(request.error))
+        System.UriBuilder uriBuilder = null;
+        var form = new WWWForm();
+        if (!urlInput.text.Contains("://"))
         {
-            AddOutput(request.text);
+            urlInput.text = "http://" + urlInput.text;
         }
-        else
+        try
         {
-            AddOutput(request.error);
+            uriBuilder = new System.UriBuilder(urlInput.text);
+        }
+        catch(System.Exception e)
+        {
+            AddOutput(e.ToString());
+        }
+        if(uriBuilder != null)
+        {
+            if (methodInput.value == 0) //GET
+            {
+                foreach (Transform keyValueGroup in paramsScrollViewContent)
+                {
+                    if (keyValueGroup.name.ToLower().Contains("keyvaluegroup"))
+                    {
+                        string key = "", value = "";
+                        foreach(Transform item in keyValueGroup)
+                        {
+                            if (item.name.ToLower().Contains("key"))
+                            {
+                                key = WWW.EscapeURL(item.GetComponent<InputField>().text);
+                            }
+                            else if (item.name.ToLower().Contains("value"))
+                            {
+                                value = WWW.EscapeURL(item.GetComponent<InputField>().text);
+                            }
+                        }
+                        string queryToAppend = string.IsNullOrEmpty(key) ? "" : (string.IsNullOrEmpty(value) ? key : string.Format("{0}={1}", key, value));
+                        if (!string.IsNullOrEmpty(queryToAppend))
+                        {
+                            if (uriBuilder.Query != null && uriBuilder.Query.Length > 1)
+                                uriBuilder.Query = uriBuilder.Query.Substring(1) + "&" + queryToAppend;
+                            else
+                                uriBuilder.Query = queryToAppend;
+                        }
+
+                    }
+                }
+            }
+            else
+            {
+                foreach (Transform keyValueGroup in paramsScrollViewContent)
+                {
+                    if (keyValueGroup.name.ToLower().Contains("keyvaluegroup"))
+                    {
+                        string key = "", value = "";
+                        foreach (Transform item in keyValueGroup)
+                        {
+                            if (item.name.ToLower().Contains("key"))
+                            {
+                                key = WWW.EscapeURL(item.GetComponent<InputField>().text);
+                            }
+                            else if (item.name.ToLower().Contains("value"))
+                            {
+                                value = WWW.EscapeURL(item.GetComponent<InputField>().text);
+                            }
+                        }
+                        if(!string.IsNullOrEmpty(key))
+                        {
+                            form.AddField(key, value);
+                        }
+                    }
+                }
+
+            }
+            Debug.Log(uriBuilder.ToString());
+            WWW request = methodInput.value == 0 ? new WWW(uriBuilder.ToString()) : new WWW(uriBuilder.ToString(), form);
+            //while(!request.isDone)
+            //{
+            //    yield return new WaitForSeconds(1);
+            //    Debug.Log("progress: " +request.progress);
+            //}
+            //Debug.Log("request.error: " + request.error + " | text: " + request.text + " | size: " + request.size);
+            yield return request;
+            if (string.IsNullOrEmpty(request.error))
+            {
+                AddOutput(request.text);
+            }
+            else
+            {
+                AddOutput(request.error);
+            }
         }
     }
 
@@ -37,15 +117,36 @@ public class GUIHandler : MonoBehaviour {
     {
         var keyValueObj = GameObject.Instantiate<GameObject>(baseKeyValueObj);
         keyValueObj.transform.SetParent(paramsScrollViewContent.transform, false);
+        foreach(Transform child in keyValueObj.transform)
+        {
+            if (child.name.ToLower().Contains("remove"))
+            {
+                child.GetComponent<Button>().onClick.AddListener(delegate { RemovePair(keyValueObj); });
+            }
+        }
     }
 
     private void AddOutput(string text)
     {
-        for(var i = 0; i < text.Length / maxCharsPerText; i++)
+        Debug.Log("text: " + text);
+        for (var i = 0; i < Mathf.Max(text.Length / maxCharsPerText, 1); i++)
         {
             var LblOutputObj = GameObject.Instantiate<GameObject>(baseLblOutput);
             LblOutputObj.GetComponent<Text>().text = text.Substring(i * maxCharsPerText, Mathf.Min((i+1) * maxCharsPerText, text.Length));
             LblOutputObj.transform.SetParent(resultScrollViewContent.transform, false);
+        }
+    }
+
+    public void RemovePair(GameObject pair)
+    {
+        GameObject.Destroy(pair);
+    }
+
+    public void ClearOutput()
+    {
+        foreach(Transform child in resultScrollViewContent)
+        {
+            GameObject.Destroy(child.gameObject);
         }
     }
 
